4179035bc24bd4dc27b411f4c21eb05f71edad0b
Sebastian <denzilbao@gmail.com>
2020-10-06 13:36:30 +0800

    [PF-04] As an User, I want a itineraries home page design.

--- web-client/src/pages/Home.vue
+++ web-client/src/pages/Home.vue
@@ -1,5 +1,27 @@
 <template>
   <v-app :style="{ backgroundColor: '#ECEFF3' }">
+    <v-app-bar
+      :color="isScrollingDown ? 'secondary' : 'transparent'"
+      :flat="!isScrollingDown"
+      fixed
+      style="z-index: 5"
+      dark
+    >
+      <v-toolbar-title class="font-weight-bold white--text">
+        Travel
+        <span :class="`${isScrollingDown ? 'primary--text' : ''}`">Buddy </span>
+      </v-toolbar-title>
+      <v-spacer> </v-spacer>
+      <v-btn color="primary" depressed class="text-capitalize white--text"
+        >Sign In</v-btn
+      >
+      <v-btn
+        :color="isScrollingDown ? 'white' : 'primary'"
+        text
+        class="text-capitalize white--text"
+        >Signup</v-btn
+      >
+    </v-app-bar>
     <v-main>
       <home-page-header></home-page-header>
       <div :style="{ marginBottom: '3rem' }"></div>
@@ -13,8 +35,8 @@
           </v-col>
         </v-row>
       </v-container>
+      <generic-basic-footer></generic-basic-footer>
     </v-main>
-    <generic-basic-footer></generic-basic-footer>
   </v-app>
 </template>
 
@@ -23,6 +45,7 @@ import HomePageHeader from "@/components/home-page/Header";
 import HomePageFeaturedItinerariesCard from "@/components/home-page/FeaturedItinerariesCard";
 import HomePageTopTourGuidesCard from "@/components/home-page/TopTourGuidesCard";
 import GenericBasicFooter from "@/components/generic/footer/Basic";
+import CommonUtilities from "@/common/utilities";
 export default {
   components: {
     GenericBasicFooter,
@@ -30,5 +53,6 @@ export default {
     HomePageFeaturedItinerariesCard,
     HomePageHeader,
   },
+  mixins: [CommonUtilities],
 };
 </script>
