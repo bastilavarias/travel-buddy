7ac9f1617556a80dc90c69b5657fc3f4aa10cbfc
Sebastian <denzilbao@gmail.com>
2020-10-06 11:03:47 +0800

    home page header design created.

---
+++ web-client/src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <home-page-header></home-page-header>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HomePageHeader from "@/components/home-page/Header";
+export default {
+  components: { HomePageHeader },
+};
+</script>
