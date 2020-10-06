e508da968e1572bf67e21097e9d8e36c109b54b4
Sebastian <denzilbao@gmail.com>
2020-10-06 11:47:49 +0800

    create featured itineraries card.

--- web-client/src/pages/Home.vue
+++ web-client/src/pages/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <v-app>
+  <v-app :style="{ backgroundColor: '#ECEFF3' }">
     <v-main>
       <home-page-header></home-page-header>
+      <div :style="{ marginBottom: '3rem' }"></div>
+      <v-container>
+        <home-page-featured-itineraries-card></home-page-featured-itineraries-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HomePageHeader from "@/components/home-page/Header";
+import HomePageFeaturedItinerariesCard from "@/components/home-page/FeaturedItinerariesCard";
 export default {
-  components: { HomePageHeader },
+  components: { HomePageFeaturedItinerariesCard, HomePageHeader },
 };
 </script>
