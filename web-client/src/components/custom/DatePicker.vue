41f7c0fdc2f34b5076c5e6bb92fa6a523c2cf008
Sebastian <denzilbao@gmail.com>
2020-10-14 14:42:30 +0800

    [PF-26] Custom date picker component design.

---
+++ web-client/src/components/custom/DatePicker.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-menu
+    v-model="menu"
+    :close-on-content-click="false"
+    :nudge-right="40"
+    transition="scale-transition"
+    offset-y
+    min-width="290px"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        :value="formattedDate"
+        :label="label"
+        :filled="filled"
+        :single-line="singleLine"
+        :rounded="rounded"
+        readonly
+        v-bind="attrs"
+        v-on="on"
+      ></v-text-field>
+    </template>
+    <v-date-picker v-model="dateLocal" @input="menu = false"></v-date-picker>
+  </v-menu>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "custom-date-picker",
+  props: {
+    date: {
+      required: true,
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+    rounded: {
+      type: Boolean,
+      required: false,
+    },
+    filled: {
+      type: Boolean,
+      required: false,
+    },
+    singleLine: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      dateLocal: this.date,
+      menu: false,
+    };
+  },
+  computed: {
+    formattedDate() {
+      return this.dateLocal
+        ? moment(this.dateLocal).format("MMMM Do YYYY")
+        : "";
+    },
+  },
+  watch: {
+    date(val) {
+      this.dateLocal = val;
+    },
+    dateLocal(val) {
+      this.$emit("update:date", val);
+    },
+  },
+};
+</script>
