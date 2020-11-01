313daaf5e2ec93b35a3b91b9bc0cc68bf54ad8a3
Sebastian <denzilbao@gmail.com>
2020-11-01 11:12:12 +0800

    [PF-54] As an Admin, I want to have a fetch itineraries functionality.

--- web-client/src/store/modules/itinerary.js
+++ web-client/src/store/modules/itinerary.js
@@ -1,8 +1,22 @@
-import { CREATE_NEW_ITINERARY } from "@/store/types/itinerary";
+import {
+  CREATE_NEW_ITINERARY,
+  FETCH_ITINERARIES,
+  SET_ITINERARIES,
+} from "@/store/types/itinerary";
 import itineraryApiService from "@/services/api/modules/itinerary";
 import { SET_GENERIC_GLOBAL_SNACKBAR_CONFIGS } from "@/store/types/generic";
 
 const itineraryStore = {
+  state: {
+    list: [],
+  },
+
+  mutations: {
+    [SET_ITINERARIES](state, itineraries) {
+      state.list = itineraries;
+    },
+  },
+
   actions: {
     async [CREATE_NEW_ITINERARY]({ commit }, form) {
       try {
@@ -27,6 +41,20 @@ const itineraryStore = {
           uploadedItineraryPost.id,
           payload
         );
+      } catch (error) {
+        commit(SET_GENERIC_GLOBAL_SNACKBAR_CONFIGS, {
+          isOpen: true,
+          text: "Something went wrong to the server. Please try again.",
+          color: "error",
+        });
+        throw new Error(`[RWV] ApiService ${error}`);
+      }
+    },
+
+    async [FETCH_ITINERARIES]({ commit }) {
+      try {
+        const itineraries = await itineraryApiService.fetch();
+        commit(SET_ITINERARIES, itineraries);
       } catch (error) {
         commit(SET_GENERIC_GLOBAL_SNACKBAR_CONFIGS, {
           isOpen: true,
