5efca847a3d4d569946d19610b0542116b51344d
Sebastian <denzilbao@gmail.com>
2021-02-18 13:04:03 +0800

    save progress.

--- src/database/seeds/index.ts
+++ src/database/seeds/index.ts
@@ -15,6 +15,10 @@ import Profile from "../entities/Profile";
 import defaultAdministratorProfile from "./modules/default-administrator/profile";
 import Account from "../entities/Account";
 import defaultAdministratorAccount from "./modules/default-administrator/account";
+import ItineraryPost from "../entities/ItineraryPost";
+import defaultItineraryPosts from "./modules/default-itineraries/post";
+import defaultItineraryPostDays from "./modules/default-itineraries/days";
+import ItineraryPostDay from "../entities/ItineraryPostDay";
 
 const generateSeeds = async () => {
   const connection = await Database.init();
@@ -73,6 +77,20 @@ const generateSeeds = async () => {
     .into(Account)
     .values(defaultAdministratorAccount)
     .onConflict(`("email") do nothing`)
+    .execute();
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(ItineraryPost)
+    .values(defaultItineraryPosts)
+    .onConflict(`("id") do nothing`)
+    .execute();
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(ItineraryPostDay)
+    .values(defaultItineraryPostDays)
+    .onConflict(`("id") do nothing`)
     .execute();
 };
 
